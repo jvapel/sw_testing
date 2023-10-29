a7f16f830a58a10705518f8c78e45e009df7f90d
Juuso <juuso.peltonen@tuni.fi>
2023-10-29 15:44:55 +0200

    testing workflow

--- my-test.js
+++ my-test.js
@@ -5,7 +5,6 @@ import add from '../test/src/add.js';
 
 import divide from '../test/src/divide.js';
 
-
 describe('SW testing course tool test', function () {
   it('should be able to add integers', function () {
     const result = add(38,4);;
