f2751ade2a4887161177d71777715e0ad1173f97
Juuso <juuso.peltonen@tuni.fi>
2023-10-29 14:02:37 +0200

    works

--- my-test.js
+++ my-test.js
@@ -1,11 +1,11 @@
 import chai from 'chai';
 import { describe, it } from 'mocha';
 
-import functions from '../src/';
+import add from '../test/src/add.js';
 
 describe('SW testing course tool test', function () {
   it('should do something', function () {
-    const result = functions.add(38,4);;
+    const result = add(38,4);;
     chai.expect(result).to.equal(42);
   });
 });
