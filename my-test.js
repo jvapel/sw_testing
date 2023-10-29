bde850dd2822f6d840a1e66427a56e7e701a242c
Juuso <juuso.peltonen@tuni.fi>
2023-10-29 21:21:10 +0200

    test

--- my-test.js
+++ my-test.js
@@ -3,7 +3,7 @@ import { describe, it } from 'mocha';
 
 import add from '../test/src/add.js';
 import divide from '../test/src/divide.js';
- 
+
 describe('SW testing course tool test', function () {
   it('should be able to add integers', function () {
     const result = add(38,4);;
