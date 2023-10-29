2aea7c1e2008a20dabfe4b733ea65c09f93ea5f6
Juuso <juuso.peltonen@tuni.fi>
2023-10-29 20:39:03 +0200

    test

--- my-test.js
+++ my-test.js
@@ -4,7 +4,6 @@ import { describe, it } from 'mocha';
 import add from '../test/src/add.js';
 import divide from '../test/src/divide.js';
 
-
 describe('SW testing course tool test', function () {
   it('should be able to add integers', function () {
     const result = add(38,4);;
@@ -12,7 +11,6 @@ describe('SW testing course tool test', function () {
   });
   it('should be able to divide integers', function () {
     const result = divide(40,10);;
-    chai.expect(result).to.equal(1);
+    chai.expect(result).to.equal(4);
   });
- 
 });
