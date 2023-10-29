699e26d2081a35aa0a3d2e9a706176eca7d36ab4
Juuso <juuso.peltonen@tuni.fi>
2023-10-29 16:00:23 +0200

    testing flow

--- my-test.js
+++ my-test.js
@@ -5,6 +5,7 @@ import add from '../test/src/add.js';
 
 import divide from '../test/src/divide.js';
 
+
 describe('SW testing course tool test', function () {
   it('should be able to add integers', function () {
     const result = add(38,4);;
@@ -12,7 +13,7 @@ describe('SW testing course tool test', function () {
   });
   it('should be able to divide integers', function () {
     const result = divide(40,10);;
-    chai.expect(result).to.equal(4);
+    chai.expect(result).to.equal(1);
   });
  
 });
