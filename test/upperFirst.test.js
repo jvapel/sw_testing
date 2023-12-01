ce575b40f3c210ee0ae27008f39e3d4994c9b317
Valtteri Asikainen <valtteri.asikainen@tuni.fi>
2023-12-01 14:26:37 +0200

    Fix test for upperCase

--- test/upperFirst.test.js
+++ test/upperFirst.test.js
@@ -6,8 +6,8 @@ import upperFirst from '../src/upperFirst.js';
 
 describe('upperFirst', function () {
 	it('should convert only the first character to upper case', function () {
-		expect(upperFirst('fred')).to.be('Fred');
-		expect(upperFirst('FRED')).to.be('FRED');
-		expect(upperFirst('Fred')).to.be('Fred');
+		expect(upperFirst('fred')).to.equal('Fred');
+		expect(upperFirst('FRED')).to.equal('FRED');
+		expect(upperFirst('Fred')).to.equal('Fred');
 	});
 });
