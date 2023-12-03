788f926b6d7b575d9b4e68930748ab23ab419df1
Juuso <juuso.peltonen@tuni.fi>
2023-12-03 14:28:39 +0200

    Add tests for map.js

---
+++ test/map.test.js
@@ -0,0 +1,25 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+
+import map from '../src/map.js';
+
+function square(n) {
+    return n * n
+}
+
+function checklength(n) {
+    return n.length;
+}
+
+describe('map', function() {
+    it('should create an array of numerical values by running each element of `array` through `iteratee', function() {
+        expect(map([4, 8], square)).to.deep.equal([16, 64])
+    });
+    it('should not throw an error when given an empty array', function() {
+        expect(map([], square)).to.deep.equal([])
+    });
+    it('should work with arrays of strings when given a suitable function', function() {
+        expect(map(["bob", "fred"], checklength)).to.deep.equal([3,4])
+    });
+
+})
