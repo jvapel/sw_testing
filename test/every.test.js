e3170d4c6de8c69df3065e314337335809ce73e2
Valtteri Asikainen <valtteri.asikainen@tuni.fi>
2023-12-06 17:16:41 +0200

    Add assertions to tests for every.js

--- test/every.test.js
+++ test/every.test.js
@@ -9,5 +9,14 @@ describe('every', function() {
     });
     it('should return false if predicate returns falsy for one elements of the array', function() {
         expect(every([true, 1, null, 'yes'], Boolean)).to.be.false
+		expect(every([true, 1, undefined], identity)).to.be.false
+		expect(every([true, 1, false], identity)).to.be.false
+		expect(every([true, 1, NaN], identity)).to.be.false
+		expect(every([true, 1, 0], identity)).to.be.false
+		expect(every([true, 1, ''], identity)).to.be.false
+		
+    });
+	it('should return true for empty arrays', function() {
+        expect(every([], Boolean)).to.be.true
     });
 })
