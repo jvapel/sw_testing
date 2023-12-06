27236ab047720971410a9dc4c57dc7f94f404818
Valtteri Asikainen <valtteri.asikainen@tuni.fi>
2023-12-06 19:43:04 +0200

    Try to fix errors in memoize.test.js

--- test/memoize.test.js
+++ test/memoize.test.js
@@ -6,7 +6,7 @@ import memoize from '../src/memoize.js';
 describe('memoize', function() {
     it('should memoize the first result', function() {
 		const object = { 'a': 1, 'b': 2 }
-		const values = memoize(values)
+		const values = memoize(Object.values)
 	
         expect(values(object)).to.deep.equal([1,2])
 		object.a = 2
@@ -14,7 +14,7 @@ describe('memoize', function() {
     });
 	it('should allow modifying cache', function() {
 		const object = { 'a': 1, 'b': 2 }
-		const values = memoize(values)
+		const values = memoize(Object.values)
 		
         expect(values(object)).to.deep.equal([1,2])
 		values.cache.set(object, ['a', 'b'])
@@ -25,6 +25,7 @@ describe('memoize', function() {
 		
         expect(memoized(1,2)).to.equal(3)
 		expect(memoized(1,3)).to.equal(3)
+		expect(memoized(2,3)).to.equal(5)
     });
 	it('should support a resolver', function() {
         const func = (a,b) => a+b
@@ -32,6 +33,7 @@ describe('memoize', function() {
 		
 		expect(memoized(1,2)).to.equal(3)
 		expect(memoized(1,3)).to.equal(4)
+		expect(memoized(2,3)).to.equal(5)
     });
 	// TODO: this binding
 	// TODO: custom cache
