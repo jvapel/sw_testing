8c4d35ecab259607dd3dc8c51bda04e9885889bc
Valtteri Asikainen <valtteri.asikainen@tuni.fi>
2023-12-06 17:35:01 +0200

    Add more falsy items to filter.test.js

--- test/filter.test.js
+++ test/filter.test.js
@@ -8,7 +8,12 @@ describe('filter', function () {
 	it('should return an array of all elements `predicate` returns truthy for', function () {
 		const users = [
 			{ 'user': 'barney', 'active': true },
-			{ 'user': 'fred',   'active': false }
+			{ 'user': 'fred',   'active': false },
+			{ 'user': 'adam',   'active': null },
+			{ 'user': 'carl',   'active': undefined },
+			{ 'user': 'dave',   'active': NaN },
+			{ 'user': 'emil',   'active': 0 },
+			{ 'user': 'gary',   'active': '' }
 		];
 		
 		expect(filter(users, ({active}) => active)).to.deep.equal(
