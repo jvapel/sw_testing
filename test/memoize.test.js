244ac5a4d45e47672520cb0bfc3d893950068d7f
Valtteri Asikainen <valtteri.asikainen@tuni.fi>
2023-12-06 19:33:40 +0200

    Add tests for memoize

---
+++ test/memoize.test.js
@@ -0,0 +1,38 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+
+import memoize from '../src/memoize.js';
+
+describe('memoize', function() {
+    it('should memoize the first result', function() {
+		const object = { 'a': 1, 'b': 2 }
+		const values = memoize(values)
+	
+        expect(values(object)).to.deep.equal([1,2])
+		object.a = 2
+		expect(values(object)).to.deep.equal([1,2])
+    });
+	it('should allow modifying cache', function() {
+		const object = { 'a': 1, 'b': 2 }
+		const values = memoize(values)
+		
+        expect(values(object)).to.deep.equal([1,2])
+		values.cache.set(object, ['a', 'b'])
+        expect(values(object)).to.deep.equal(['a', 'b'])
+    });
+	it('should use the first argument as the cache key', function() {
+		const memoized = memoize((a,b) => a+b)
+		
+        expect(memoized(1,2)).to.equal(3)
+		expect(memoized(1,3)).to.equal(3)
+    });
+	it('should support a resolver', function() {
+        const func = (a,b) => a+b
+		const memoized = memoize(func, func)
+		
+		expect(memoized(1,2)).to.equal(3)
+		expect(memoized(1,3)).to.equal(4)
+    });
+	// TODO: this binding
+	// TODO: custom cache
+})
