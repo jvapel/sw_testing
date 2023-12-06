c05a10565b673acfae4353c66ecc0c6ad7daeb12
Juuso <juuso.peltonen@tuni.fi>
2023-12-06 13:44:08 +0200

    Add basic tests for reduce.js

---
+++ test/reduce.test.js
@@ -0,0 +1,15 @@
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+
+import reduce from '../src/reduce.js';
+
+describe('reduce', function() {
+    it('should reduce an array to a value which is the accumulated result of running each element in collection through iteratee', function() {
+        expect(reduce([1, 2], (sum, n) => sum + n, 0)).to.equal(3)
+    });
+    it('should reduce an object to an object which has the accumulated result of running each element in collection through iteratee', function() {
+        expect(reduce({ 'a': 1, 'b': 2, 'c': 1 }, (result, value, key) => {
+            (result[value] || (result[value] = [])).push(key)
+            return result}, {})).to.deep.equal({ '1': ['a', 'c'], '2': ['b'] })
+    });
+})
